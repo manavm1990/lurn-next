f85f34f78fedf53212e76eadd0ec7247d4b20de4
Manav Misra <manavm@visionwebsoft.com>
2023-06-07 22:44:20 -0500

    :label: Simplify types

--- app/types/note.types.ts
+++ app/types/note.types.ts
@@ -1,14 +1,22 @@
 import { z } from 'zod';
 
-const NoteSchema = z.object({
-  collectionId: z.string(),
-  collectionName: z.string(),
-  content: z.string(),
-  created: z.string(),
-  id: z.string(),
-  title: z.string(),
-  updated: z.string(),
-});
+const NoteSchema = z
+  .object({
+    collectionId: z.string(),
+    collectionName: z.string(),
+
+    content: z.string().min(2, { message: 'Need at least 2 characters.' }),
+    created: z.string(),
+    id: z.string(),
+    title: z.string().min(2, { message: 'Need at least 2 characters.' }),
+    updated: z.string(),
+  })
+
+  // ...ensure that the object being validated matches your schema exactly, with no missing or additional fields.
+  .required()
+  .strict();
+
+export const NewNoteSchema = NoteSchema.pick({ title: true, content: true });
 
 export type NewNoteType = Pick<NoteType, 'title' | 'content'>;
 export type NoteParamsType = Pick<NoteType, 'id'>;
